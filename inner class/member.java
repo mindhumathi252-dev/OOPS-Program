234bbb02557a26b25c65b09ff8640beb0e9ba7c8
mindhumathi252-dev <mindhumathi252@gmail.com>
2026-08-21 14:54:08 +0530

    member class

--- inner class/member.java
+++ inner class/member.java
@@ -1,1 +1,15 @@
+class College {
+    private String name = "ABC College";
 
+    class Student {
+        void display() {
+            System.out.println("College Name: " + name);
+        }
+    }
+
+    public static void main(String[] args) {
+        College obj = new College();
+        College.Student s = obj.new Student();
+        s.display();
+    }
+}
